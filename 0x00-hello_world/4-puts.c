7599a996ec25a320bb1089b3e56fb8657036279e
Evans Obi <vnsobi@gmail.com>
2022-09-08 07:18:56 -0700

    task 0 to 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* A program that prints "programming is like bulding a multilingual puzzle"
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingal puzzle");
+	return (0);
+}
