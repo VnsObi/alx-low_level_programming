cbdad56d03b27619ced194b9d93af4d4677bc9c4
Evans Obi <32423919+VnsObi@users.noreply.github.com>
2022-09-14 13:22:57 +0100

    Update 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - pa function that prints 10 times the alphabet, in lowercase, followed by a new line.
+ */
+ 
+void print_alphabet_x10(void)
+{
+	char al;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		al = 'a';
+		while (al <= 'z')
+		{
+			_putchar(ch);
+			al++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
