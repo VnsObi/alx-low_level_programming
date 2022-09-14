2df7e45692f704dd71ef8ce99820a2a73c574d5a
Evans Obi <32423919+VnsObi@users.noreply.github.com>
2022-09-14 13:13:30 +0100

    Update 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - a function that prints the alphabet, in lowercase, followed by a new line.
+ */
+ 
+void print_alphabet(void)
+{
+	char al;
+
+	al = 'a';
+
+	while (al <= 'z')
+	{
+		_putchar(al);
+		al++;
+	}
+
+	_putchar('\n');
+}
