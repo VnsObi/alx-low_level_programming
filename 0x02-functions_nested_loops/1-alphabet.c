662fdecf658fcacdb989b9f6f9ea9474a60ed93f
Evans Obi <32423919+VnsObi@users.noreply.github.com>
2022-09-22 14:42:44 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- * main - a function that prints the alphabet, in lowercase, followed by a new line.
+ * main - a function that prints the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ * Return: 0
  */
  
 void print_alphabet(void)
