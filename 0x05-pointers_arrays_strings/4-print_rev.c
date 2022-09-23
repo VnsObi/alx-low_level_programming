3928ab2682f87793d939972e5dabe992e6994427
Evans Obi <32423919+VnsObi@users.noreply.github.com>
2022-09-23 14:01:14 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_rev - a function that prints a string, in reverse, followed by a new line.
+ * print_rev - a function that prints a string
+ * in reverse, followed by a new line.
+ * @s: string
  */
- 
 void print_rev(char *s)
 {
 	int i, j, len;
